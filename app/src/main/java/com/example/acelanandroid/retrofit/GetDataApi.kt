7a7eec9f8b99b9e53f6aee24ef8b27632e5b90b8
pav3l-abramov <102557961+pav3l-abramov@users.noreply.github.com>
2024-01-16 21:11:32 +0300

    Update GetDataApi.kt

--- app/src/main/java/com/example/acelanandroid/retrofit/GetDataApi.kt
+++ app/src/main/java/com/example/acelanandroid/retrofit/GetDataApi.kt
@@ -3,13 +3,14 @@ package com.example.acelanandroid.retrofit
 import com.example.acelanandroid.retrofit.data.TaskDetails
 import com.example.acelanandroid.retrofit.data.Tasks
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
 
 interface GetDataApi {
     @GET("/api/tasks/")
-    suspend fun getTasks():Tasks
+    suspend fun getTasks(@Header("Authorization") token:String):Tasks
 
     @GET("/api/tasks/{id}")
-    fun getTaskDetails(@Path("id") id:Int):TaskDetails
+    fun getTaskDetails(@Header("Authorization") token:String, @Path("id") id:Int):TaskDetails
 }
 
